81d5e720aca2591a593761db609b68a98eb398b0
Jhon Alex García Álvarez <Jhon Alex Garca lvarez>
2017-03-29 08:53:09 -0500

    Primera version del proyecto

---
+++ app/src/main/java/com/example/acerpc/appmiercoles/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.acerpc.appmiercoles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView vista1;
+    Button Button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        vista1 = (TextView) findViewById(R.id.vista1);
+        Button1 = (Button) findViewById(R.id.button);
+        Button1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        vista1.setText("Cambiamos el texto con el boton");
+        vista1.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+    }
+}
